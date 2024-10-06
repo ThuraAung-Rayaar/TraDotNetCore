a20ddb2739afaf5fc64567b2711d4c9be408dfc7
RaYaar <thurayaaung2003@gmail.com>
2024-10-06 21:14:55 +0630

    feat: lesson 5

---
+++ traDotnetTrainingBatch5.RestAPI/Controllers/BlogsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace traDotnetTrainingBatch5.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetBlogs()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateBlogs()
+        {
+            return Ok();
+        }
+
+        [HttpPatch]
+        public IActionResult PatchBlogs()
+        {
+            return Ok();
+        }
+        [HttpPost]
+        public IActionResult CreateBlogs()
+        {
+            return Ok();
+        }
+        [HttpDelete]
+        public IActionResult DeleteBlogs()
+        {
+            return Ok();
+        }
+    }
+}
